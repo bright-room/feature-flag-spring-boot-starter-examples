41dbec3f9818a63efbba5e6678509cf0f5a3226f
kukv <rio.by.kukv@gmail.com>
2025-02-25 02:24:47 +0900

    code format

--- webflux/external-datasource/src/main/java/net/brightroom/example/FeatureFlagExternalDataSourceProvider.java
+++ webflux/external-datasource/src/main/java/net/brightroom/example/FeatureFlagExternalDataSourceProvider.java
@@ -1,11 +1,8 @@
 package net.brightroom.example;
 
-import java.util.Objects;
 import net.brightroom.featureflag.provider.FeatureFlagProvider;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @Component
 class FeatureFlagExternalDataSourceProvider implements FeatureFlagProvider {
@@ -15,9 +12,9 @@ class FeatureFlagExternalDataSourceProvider implements FeatureFlagProvider {
   @Override
   public Mono<Boolean> isFeatureEnabled(String featureName) {
     return featureManagementRepository
-            .findById(featureName)
-            .defaultIfEmpty(new FeatureManagement(featureName, true))
-            .map(FeatureManagement::enabled);
+        .findById(featureName)
+        .defaultIfEmpty(new FeatureManagement(featureName, true))
+        .map(FeatureManagement::enabled);
   }
 
   FeatureFlagExternalDataSourceProvider(FeatureManagementRepository featureManagementRepository) {
